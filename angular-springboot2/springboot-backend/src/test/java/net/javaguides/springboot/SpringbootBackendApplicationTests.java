e33da12c18c8816ea39e8d909e5b8abef7d0ae5d
Jonbram99 <jonbram99@gmail.com>
2023-12-17 19:00:25 +0100

    springboot2-with-progresssql

---
+++ angular-springboot2/springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplicationTests {
+
+
+public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplicationTests.class, args);
+	}
+
+}
